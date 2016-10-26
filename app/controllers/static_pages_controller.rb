4bcb9b1665aa415f199f3d2fa6dac2d86210cbba
Kim <dijk.kim.van@gmail.com>
2016-10-26 15:05:28 +0200

    3.8 added contact and about pages and updated index with nav

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,4 @@
+class StaticPagesController < ApplicationController
+  def indes
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  root 'static_pages#index'
+	
+  get 'static_pages/index'
+
+  get 'static_pages/indes'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
