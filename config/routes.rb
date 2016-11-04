3462f7bd20df84cd791d9049fd8dc64349f2db8a
Kim <dijk.kim.van@gmail.com>
2016-11-04 15:00:09 +0100

    3.9 styled header and footer-added images

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-
   root 'static_pages#index'
-	
+
   get 'static_pages/index'
 
   get 'static_pages/indes'
