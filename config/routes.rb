fd58ccfefd0f0bddfe2476c868ae604cfd6825bc
Kim <dijk.kim.van@gmail.com>
2016-11-18 16:41:54 +0100

    4.4 added order.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-  get 'static_pages/indes'
-
   resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
