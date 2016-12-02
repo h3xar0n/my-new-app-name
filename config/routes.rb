e1bac4be856789e6de968b396b06776c66db4b6d
Kim <dijk.kim.van@gmail.com>
2016-12-02 11:35:01 +0100

    4.6 made more pretty

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  
   resources :products
+
+  resources :orders, only: [:index, :show, :create, :destroy]
+
   root 'static_pages#landing_page'
 
   get 'static_pages/index'
 
-  resources :orders, only: [:index, :show, :create, :destroy]
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/landing_page'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
