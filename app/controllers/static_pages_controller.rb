e5d3d4c458ac3b31d10ae39a45fdac460cad27b5
Kim <dijk.kim.van@gmail.com>
2016-12-09 16:53:40 +0100

    5.3 added mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,17 @@ class StaticPagesController < ApplicationController
   def index
   end
 
-def landing_page
-  @products = Product.limit(4)
-end
+	def landing_page
+		@products = Product.limit(4)
+	end
 
+	def thank_you
+ 	 	@name = params[:name]
+ 		@email = params[:email]
+ 	 	@message = params[:message]
+ 	 	ActionMailer::Base.mail(:from => @email,
+      	:to => 'info@dutchkdesign.nl',
+     		:subject => "A new contact form message from #{@name}",
+      	:body => @message).deliver_now
+	end
 end
