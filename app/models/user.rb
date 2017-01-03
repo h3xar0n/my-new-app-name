92a1654559f0d139a2e561e5b0f7a5af986fd1af
Kim <dijk.kim.van@gmail.com>
2017-01-03 10:40:16 +0100

    5.10 added admin flag

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	include CanCan::Ability
+
 	validates :email, uniqueness: true
 	
   # Include default devise modules. Others available are:
@@ -6,4 +8,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :orders
+
+	def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :create, :all
+    end
+  end
 end
