da7966869bc85953cf9eb2e9db11cedaa576a241
Kim <dijk.kim.van@gmail.com>
2017-01-16 10:57:49 +0100

    6.10 AJAX added for comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,14 +1,21 @@
-$(document).on('turbolinks:load', function(){
-	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-	$('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
-  console.log($(".alert"));
-  $(".alert").delay(4000).slideUp(3000).fadeOut("slow");
+var refreshRating = function() {
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '/assets',
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
+    }
+  });
+};
+
+$(document).on('turbolinks:load ajaxSucces', function(){
+	refreshRating();
 
   /* elevate zoom */
   $(".img-zoom").elevateZoom({tint:true, tintColour:'black', tintOpacity:0.5, scrollZoom:true});
+
+    /* notification */
+  console.log($(".alert"));
+  $(".alert").delay(4000).slideUp(3000).fadeOut("slow");
+
 });
