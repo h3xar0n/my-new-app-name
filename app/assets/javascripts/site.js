7e21718cea7927917e33bc680c4479582f2e31e2
Kim <dijk.kim.van@gmail.com>
2017-01-19 11:45:37 +0100

    6.10 fixed raty after AJAX

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,11 +1,11 @@
 var refreshRating = function() {
-  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-  $('.rated').raty({ path: '/assets',
-    readOnly: true,
-    score: function() {
-      return $(this).attr('data-score');
-    }
-  });
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
+    });
 };
 
 $(document).on('turbolinks:load ajaxSucces', function(){
